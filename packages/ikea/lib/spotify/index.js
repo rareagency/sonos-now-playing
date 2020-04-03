37ab031d898f70d5fea6bae83a5c8f72e72a6827
Riku Rouvila <ext.riku.rouvila@tieto.com>
2020-04-03 14:48:02 +0300

    add auto refreshing for spotify token

--- packages/ikea/lib/spotify/index.js
+++ packages/ikea/lib/spotify/index.js
@@ -3,13 +3,35 @@ const fetch = require("node-fetch");
 const spotifyApiUrl = "https://api.spotify.com/v1/audio-features/{id}";
 
 module.exports = async function getSongDetails(songId) {
-  // const token = await fetch()
+  const params = {
+    grant_type: "refresh_token",
+    refresh_token:
+      "AQDYHT90I7vkU01HP6BqsW4IFVKJ9y21IfPhQoS9WPxETyO7tKMcc1ZITQssS3KVCHLyMne-21lMFISu5kKzSfIDHytcUIHv6FST_-MS-GmfwGysaovpuYUUfhSaKUapvos"
+  };
+
+  const searchParams = Object.keys(params)
+    .map(key => {
+      return encodeURIComponent(key) + "=" + encodeURIComponent(params[key]);
+    })
+    .join("&");
+
+  const tokenResponse = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      authorization:
+        "Basic YmI4ZTgzNTgyZGNjNDRmOTk1ZjY4MDFkNTAzZWYzZTM6NWJiOWU5NzNmYzBiNGI4OGJiNTFlNTFkMTYyNzk4ZDU="
+    },
+    body: searchParams
+  });
+
+  const { access_token } = await tokenResponse.json();
 
   const response = await fetch(
     `https://api.spotify.com/v1/audio-features/${songId}`,
     {
       headers: {
-        Authorization: `Bearer BQAm0NieQuK2oLZrSbaEfWEYtwa4odQoSrOGHtgRR1vQ7imXUi5y4DW16nVUtwsWeCGQzSYw1ZD8npU0XCBhMwsYwZ2A3JbVwrcVT2rX3E1Lrws0hK2ZkSrhf0cKb6ivTjw1IAQOzKVw1c4`
+        Authorization: `Bearer ${access_token}`
       }
     }
   );
