55b6eb836ecaba5652041059d96b8377e5b3be87
Riku Rouvila <riku.rouvila@gmail.com>
2021-08-28 15:00:24 +0300

    only have the now playing screen in this repo

--- packages/ikea/lib/patterns/index.js
+++
@@ -1,6 +0,0 @@
-const strobo = require("./strobo");
-const smooth = require("./smooth");
-const party = require("./party");
-const police = require("./police");
-
-module.exports = [police, party, strobo, smooth];
